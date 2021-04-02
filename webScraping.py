bd61488f0d5e6a80f13267e9b70a77ad424cd75e
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-02 12:23:42 +0530

    Completed

--- webScraping.py
+++ webScraping.py
@@ -3,4 +3,11 @@ from bs4 import BeautifulSoup as bs
 
 github_user = input("Enter github username: ")
 url = 'https://github.com/'+github_user
+# requesting server
 req = requests.get(url)
+# converting website to html
+soup = bs(req.content, 'html.parser')
+# selecting particular component from htlm
+profile_image = soup.find('img',{'alt': 'Avatar'})['src']
+# just printing image
+print(profile_image)
